0e0340b7410928b709575fc17d032371d56aad46
bhushan4work <bhushan.agrawal4work@gmail.com>
2026-01-17 19:53:52 +0530

    added new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, registerUser, logoutUser, refreshAccessToken} from "../controllers/user.controllers.js";
+import {loginUser, registerUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
@@ -26,8 +26,14 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser) //verifyJWT verifies if user is loggedIn 
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails) //patch updates only the fields you send
+router.route("/avatar").patch(verifyJWT, upload.single("avatar") ,updateUserAvatar) //single is used as one file is updated only
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage") ,updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // ' /c/: ' diff path is written due to params
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router
